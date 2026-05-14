tyrad/Prowlmr

Add Close Window command and show main window in Window menu
Add a WindowCommands file with a Cmd+W Close Window that defers to
Ghostty close_surface when overlapping. Add a "Supacode" item to
the Window menu so the main window can be reopened after closing.

supacode/App/supacodeApp.swift
@@ -177,6 +177,7 @@ struct SupacodeApp: App {
       WorktreeCommands(store: store)
       SidebarCommands()
       TerminalCommands(ghosttyShortcuts: ghosttyShortcuts)
+      WindowCommands(ghosttyShortcuts: ghosttyShortcuts)
       CommandGroup(after: .textEditing) {
         Button("Command Palette") {
           store.send(.commandPalette(.togglePresented))
@@ -186,6 +187,15 @@ struct SupacodeApp: App {
       }
       UpdateCommands(store: store.scope(state: \.updates, action: \.updates))
       CommandGroup(replacing: .windowArrangement) {
+        Button("Supacode") {
+          if let window = NSApp.windows.first(where: { $0.identifier?.rawValue == "main" }) {
+            window.makeKeyAndOrderFront(nil)
+            NSApp.activate(ignoringOtherApps: true)
+          }
+        }
+        .keyboardShortcut("0")
+        .help("Show main window (⌘0)")
+        Divider()
         Button("Minimize") {
           NSApp.keyWindow?.miniaturize(nil)
         }

pacificleo/prs-code

cherrylily/App/supacodeApp.swift
@@ -168,6 +168,7 @@ struct CherryLilyApp: App {
       WorktreeCommands(store: store)
       SidebarCommands()
       TerminalCommands(ghosttyShortcuts: ghosttyShortcuts)
+      WindowCommands(ghosttyShortcuts: ghosttyShortcuts)
       CommandGroup(after: .textEditing) {
         Button("Command Palette") {
           store.send(.commandPalette(.togglePresented))
@@ -177,6 +178,15 @@ struct CherryLilyApp: App {
       }
       UpdateCommands(store: store.scope(state: \.updates, action: \.updates))
       CommandGroup(replacing: .windowArrangement) {
+        Button("CherryLily") {
+          if let window = NSApp.windows.first(where: { $0.identifier?.rawValue == "main" }) {
+            window.makeKeyAndOrderFront(nil)
+            NSApp.activate(ignoringOtherApps: true)
+          }
+        }
+        .keyboardShortcut("0")
+        .help("Show main window (⌘0)")
+        Divider()
         Button("Minimize") {
           NSApp.keyWindow?.miniaturize(nil)
         }