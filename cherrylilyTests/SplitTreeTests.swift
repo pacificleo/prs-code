MelvinTo/supacode

Match Ghostty pane focus after close

supacodeTests/SplitTreeTests.swift
@@ -1,0 +1,37 @@
+import AppKit
+import Testing
+
+@testable import supacode
+
+@MainActor
+struct SplitTreeTests {
+  @Test func focusTargetAfterClosingUsesNextForLeftmostLeaf() throws {
+    let first = SplitTreeTestView()
+    let second = SplitTreeTestView()
+    let third = SplitTreeTestView()
+
+    let tree = try SplitTree(view: first)
+      .inserting(view: second, at: first, direction: .right)
+      .inserting(view: third, at: second, direction: .right)
+
+    let node = try #require(tree.find(id: first.id))
+    #expect(tree.focusTargetAfterClosing(node) === second)
+  }
+
+  @Test func focusTargetAfterClosingUsesPreviousForNonLeftmostLeaf() throws {
+    let first = SplitTreeTestView()
+    let second = SplitTreeTestView()
+    let third = SplitTreeTestView()
+
+    let tree = try SplitTree(view: first)
+      .inserting(view: second, at: first, direction: .right)
+      .inserting(view: third, at: second, direction: .right)
+
+    let node = try #require(tree.find(id: third.id))
+    #expect(tree.focusTargetAfterClosing(node) === second)
+  }
+}
+
+private final class SplitTreeTestView: NSView, Identifiable {
+  let id = UUID()
+}

pacificleo/prs-code

cherrylilyTests/SplitTreeTests.swift
@@ -1,0 +1,37 @@
+import AppKit
+import Testing
+
+@testable import CherryLily
+
+@MainActor
+struct SplitTreeTests {
+  @Test func focusTargetAfterClosingUsesNextForLeftmostLeaf() throws {
+    let first = SplitTreeTestView()
+    let second = SplitTreeTestView()
+    let third = SplitTreeTestView()
+
+    let tree = try SplitTree(view: first)
+      .inserting(view: second, at: first, direction: .right)
+      .inserting(view: third, at: second, direction: .right)
+
+    let node = try #require(tree.find(id: first.id))
+    #expect(tree.focusTargetAfterClosing(node) === second)
+  }
+
+  @Test func focusTargetAfterClosingUsesPreviousForNonLeftmostLeaf() throws {
+    let first = SplitTreeTestView()
+    let second = SplitTreeTestView()
+    let third = SplitTreeTestView()
+
+    let tree = try SplitTree(view: first)
+      .inserting(view: second, at: first, direction: .right)
+      .inserting(view: third, at: second, direction: .right)
+
+    let node = try #require(tree.find(id: third.id))
+    #expect(tree.focusTargetAfterClosing(node) === second)
+  }
+}
+
+private final class SplitTreeTestView: NSView, Identifiable {
+  let id = UUID()
+}