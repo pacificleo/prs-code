onevcat/supacode

Populate environment variables in all scripts
Inject SUPACODE_WORKTREE_PATH and SUPACODE_ROOT_PATH into setup, run,
and archive scripts via export prefixes. Add a settings UI section
listing the available variables with descriptions and values.

supacode/Domain/Worktree.swift
@@ -24,3 +24,21 @@ struct Worktree: Identifiable, Hashable, Sendable {
     self.createdAt = createdAt
   }
 }
+
+extension Worktree {
+  /// Environment variables exposed to all Supacode scripts.
+  var scriptEnvironment: [String: String] {
+    [
+      "SUPACODE_WORKTREE_PATH": workingDirectory.path(percentEncoded: false),
+      "SUPACODE_ROOT_PATH": repositoryRootURL.path(percentEncoded: false),
+    ]
+  }
+
+  /// Shell export statements for prepending to scripts.
+  var scriptEnvironmentExportPrefix: String {
+    scriptEnvironment
+      .sorted(by: { $0.key < $1.key })
+      .map { "export \($0.key)='\($0.value.replacing("'", with: "'\"'\"'"))'" }
+      .joined(separator: "\n") + "\n"
+  }
+}

supacode/Features/Settings/Views/RepositorySettingsView.swift
@@ -111,6 +111,23 @@ struct RepositorySettingsView: View {
         }
       }
       Section {
+        ScriptEnvironmentRow(
+          name: "SUPACODE_WORKTREE_PATH",
+          description: "Path to the active worktree."
+        )
+        ScriptEnvironmentRow(
+          name: "SUPACODE_ROOT_PATH",
+          value: store.rootURL.path(percentEncoded: false),
+          description: "Path to the repository root."
+        )
+      } header: {
+        VStack(alignment: .leading, spacing: 4) {
+          Text("Environment Variables")
+          Text("Exported in all scripts below")
+            .foregroundStyle(.secondary)
+        }
+      }
+      Section {
         ZStack(alignment: .topLeading) {
           PlainTextEditor(
             text: settings.setupScript
@@ -242,5 +259,25 @@ private struct BranchPickerPopover: View {
     }
     .frame(width: 300, height: 350)
     .onAppear { isSearchFocused = true }
+  }
+}
+
+private struct ScriptEnvironmentRow: View {
+  let name: String
+  var value: String?
+  let description: String
+
+  var body: some View {
+    VStack(alignment: .leading, spacing: 2) {
+      Text(name)
+        .monospaced()
+      if let value {
+        Text(value)
+          .foregroundStyle(.secondary)
+          .monospaced()
+      }
+      Text(description)
+        .foregroundStyle(.tertiary)
+    }
   }
 }

supacodeTests/WorktreeEnvironmentTests.swift
@@ -1,0 +1,64 @@
+import Foundation
+import Testing
+
+@testable import supacode
+
+@MainActor
+struct WorktreeEnvironmentTests {
+  @Test func scriptEnvironmentContainsExpectedKeys() {
+    let worktree = Worktree(
+      id: "/tmp/repo/wt-1",
+      name: "feature-branch",
+      detail: "detail",
+      workingDirectory: URL(fileURLWithPath: "/tmp/repo/wt-1"),
+      repositoryRootURL: URL(fileURLWithPath: "/tmp/repo"),
+    )
+    let env = worktree.scriptEnvironment
+    #expect(env["SUPACODE_WORKTREE_PATH"] == "/tmp/repo/wt-1")
+    #expect(env["SUPACODE_ROOT_PATH"] == "/tmp/repo")
+    #expect(env.count == 2)
+  }
+
+  @Test func exportPrefixFormatsCorrectly() {
+    let worktree = Worktree(
+      id: "/tmp/repo/wt-1",
+      name: "feature-branch",
+      detail: "detail",
+      workingDirectory: URL(fileURLWithPath: "/tmp/repo/wt-1"),
+      repositoryRootURL: URL(fileURLWithPath: "/tmp/repo/.bare"),
+    )
+    let exports = worktree.scriptEnvironmentExportPrefix
+    #expect(exports.contains("export SUPACODE_WORKTREE_PATH='/tmp/repo/wt-1'"))
+    #expect(exports.contains("export SUPACODE_ROOT_PATH='/tmp/repo/.bare'"))
+    #expect(exports.hasSuffix("\n"))
+  }
+
+  @Test func exportPrefixIsSortedByKey() {
+    let worktree = Worktree(
+      id: "/tmp/repo/wt-1",
+      name: "feature-branch",
+      detail: "detail",
+      workingDirectory: URL(fileURLWithPath: "/tmp/repo/wt-1"),
+      repositoryRootURL: URL(fileURLWithPath: "/tmp/repo/.bare"),
+    )
+    let lines = worktree.scriptEnvironmentExportPrefix
+      .trimmingCharacters(in: .newlines)
+      .components(separatedBy: "\n")
+    #expect(lines.count == 2)
+    #expect(lines[0].contains("SUPACODE_ROOT_PATH"))
+    #expect(lines[1].contains("SUPACODE_WORKTREE_PATH"))
+  }
+
+  @Test func exportPrefixQuotesPathsWithSpaces() {
+    let worktree = Worktree(
+      id: "/tmp/my repo/wt 1",
+      name: "feature-branch",
+      detail: "detail",
+      workingDirectory: URL(fileURLWithPath: "/tmp/my repo/wt 1"),
+      repositoryRootURL: URL(fileURLWithPath: "/tmp/my repo/.bare"),
+    )
+    let exports = worktree.scriptEnvironmentExportPrefix
+    #expect(exports.contains("export SUPACODE_WORKTREE_PATH='/tmp/my repo/wt 1'"))
+    #expect(exports.contains("export SUPACODE_ROOT_PATH='/tmp/my repo/.bare'"))
+  }
+}

pacificleo/prs-code

cherrylily/Domain/Worktree.swift
@@ -24,3 +24,21 @@ struct Worktree: Identifiable, Hashable, Sendable {
     self.createdAt = createdAt
   }
 }
+
+extension Worktree {
+  /// Environment variables exposed to all Supacode scripts.
+  var scriptEnvironment: [String: String] {
+    [
+      "CHERRYLILY_WORKTREE_PATH": workingDirectory.path(percentEncoded: false),
+      "CHERRYLILY_ROOT_PATH": repositoryRootURL.path(percentEncoded: false),
+    ]
+  }
+
+  /// Shell export statements for prepending to scripts.
+  var scriptEnvironmentExportPrefix: String {
+    scriptEnvironment
+      .sorted(by: { $0.key < $1.key })
+      .map { "export \($0.key)='\($0.value.replacing("'", with: "'\"'\"'"))'" }
+      .joined(separator: "\n") + "\n"
+  }
+}

cherrylily/Features/Settings/Views/RepositorySettingsView.swift
@@ -111,6 +111,23 @@ struct RepositorySettingsView: View {
         }
       }
       Section {
+        ScriptEnvironmentRow(
+          name: "CHERRYLILY_WORKTREE_PATH",
+          description: "Path to the active worktree."
+        )
+        ScriptEnvironmentRow(
+          name: "CHERRYLILY_ROOT_PATH",
+          value: store.rootURL.path(percentEncoded: false),
+          description: "Path to the repository root."
+        )
+      } header: {
+        VStack(alignment: .leading, spacing: 4) {
+          Text("Environment Variables")
+          Text("Exported in all scripts below")
+            .foregroundStyle(.secondary)
+        }
+      }
+      Section {
         ZStack(alignment: .topLeading) {
           PlainTextEditor(
             text: settings.setupScript
@@ -242,5 +259,25 @@ private struct BranchPickerPopover: View {
     }
     .frame(width: 300, height: 350)
     .onAppear { isSearchFocused = true }
+  }
+}
+
+private struct ScriptEnvironmentRow: View {
+  let name: String
+  var value: String?
+  let description: String
+
+  var body: some View {
+    VStack(alignment: .leading, spacing: 2) {
+      Text(name)
+        .monospaced()
+      if let value {
+        Text(value)
+          .foregroundStyle(.secondary)
+          .monospaced()
+      }
+      Text(description)
+        .foregroundStyle(.tertiary)
+    }
   }
 }

cherrylilyTests/WorktreeEnvironmentTests.swift
@@ -1,0 +1,64 @@
+import Foundation
+import Testing
+
+@testable import CherryLily
+
+@MainActor
+struct WorktreeEnvironmentTests {
+  @Test func scriptEnvironmentContainsExpectedKeys() {
+    let worktree = Worktree(
+      id: "/tmp/repo/wt-1",
+      name: "feature-branch",
+      detail: "detail",
+      workingDirectory: URL(fileURLWithPath: "/tmp/repo/wt-1"),
+      repositoryRootURL: URL(fileURLWithPath: "/tmp/repo"),
+    )
+    let env = worktree.scriptEnvironment
+    #expect(env["CHERRYLILY_WORKTREE_PATH"] == "/tmp/repo/wt-1")
+    #expect(env["CHERRYLILY_ROOT_PATH"] == "/tmp/repo")
+    #expect(env.count == 2)
+  }
+
+  @Test func exportPrefixFormatsCorrectly() {
+    let worktree = Worktree(
+      id: "/tmp/repo/wt-1",
+      name: "feature-branch",
+      detail: "detail",
+      workingDirectory: URL(fileURLWithPath: "/tmp/repo/wt-1"),
+      repositoryRootURL: URL(fileURLWithPath: "/tmp/repo/.bare"),
+    )
+    let exports = worktree.scriptEnvironmentExportPrefix
+    #expect(exports.contains("export CHERRYLILY_WORKTREE_PATH='/tmp/repo/wt-1'"))
+    #expect(exports.contains("export CHERRYLILY_ROOT_PATH='/tmp/repo/.bare'"))
+    #expect(exports.hasSuffix("\n"))
+  }
+
+  @Test func exportPrefixIsSortedByKey() {
+    let worktree = Worktree(
+      id: "/tmp/repo/wt-1",
+      name: "feature-branch",
+      detail: "detail",
+      workingDirectory: URL(fileURLWithPath: "/tmp/repo/wt-1"),
+      repositoryRootURL: URL(fileURLWithPath: "/tmp/repo/.bare"),
+    )
+    let lines = worktree.scriptEnvironmentExportPrefix
+      .trimmingCharacters(in: .newlines)
+      .components(separatedBy: "\n")
+    #expect(lines.count == 2)
+    #expect(lines[0].contains("CHERRYLILY_ROOT_PATH"))
+    #expect(lines[1].contains("CHERRYLILY_WORKTREE_PATH"))
+  }
+
+  @Test func exportPrefixQuotesPathsWithSpaces() {
+    let worktree = Worktree(
+      id: "/tmp/my repo/wt 1",
+      name: "feature-branch",
+      detail: "detail",
+      workingDirectory: URL(fileURLWithPath: "/tmp/my repo/wt 1"),
+      repositoryRootURL: URL(fileURLWithPath: "/tmp/my repo/.bare"),
+    )
+    let exports = worktree.scriptEnvironmentExportPrefix
+    #expect(exports.contains("export CHERRYLILY_WORKTREE_PATH='/tmp/my repo/wt 1'"))
+    #expect(exports.contains("export CHERRYLILY_ROOT_PATH='/tmp/my repo/.bare'"))
+  }
+}