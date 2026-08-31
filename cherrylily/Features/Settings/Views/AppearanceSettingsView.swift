tftio/supacode

Redesign settings
- Replace appearance cards with Liquid Glass images from macOS Tahoe
- Rename "System" to "Auto", use NSApp.appearance for reliable
  window chrome updates on mode switch
- Add Supacode Dark/Light terminal themes using macOS system colors
- Load bundled themes via Ghostty config, gated by new "Sync with
  Terminal" setting (defaults off)
- Move analytics from Advanced into General
- Reorganize all settings panes with label+subtitle toggle pattern
- Replace branch popover with system Picker in repository settings
- Add GitHub mark SVG icon for sidebar
- Clean up deleted views (SettingsDetailView, WindowAppearanceSetter,
  WindowLevelSetter, SettingsWindowManager, SettingsWindowClient)
- Make notification badge orange

supacode/Features/Settings/Views/AppearanceSettingsView.swift
@@ -6,10 +6,10 @@ struct AppearanceSettingsView: View {
 
   var body: some View {
     let openActionOptions = OpenWorktreeAction.availableCases
-    VStack(alignment: .leading) {
-      Form {
-        Section("Appearance") {
-          HStack {
+    Form {
+      Section {
+        LabeledContent("Appearance") {
+          HStack(spacing: 12) {
             let appearanceMode = $store.appearanceMode
             ForEach(AppearanceMode.allCases) { mode in
               AppearanceOptionCardView(
@@ -20,40 +20,63 @@ struct AppearanceSettingsView: View {
               }
             }
           }
+        }
+        Toggle(isOn: $store.terminalThemeSyncEnabled) {
+          Text("Sync with Terminal")
+          Text("Applies the appearance-aware Supacode color palette.")
+        }
+        if !store.terminalThemeSyncEnabled {
           VStack(alignment: .leading, spacing: 4) {
-            Text("Terminal theming follows Ghostty config")
-            Text("For example, add the following line to `~/.config/ghostty/config`")
-            Text("theme = light:Monokai Pro Light Sun,dark:Dimmed Monokai")
-              .monospaced()
+            Text("Add a theme to `~/.config/ghostty/config`")
+            Text("e.g. `theme = light:Monokai Pro Light Sun,dark:Dimmed Monokai`")
           }
           .font(.footnote)
           .foregroundStyle(.secondary)
           .textSelection(.enabled)
         }
-        Section("Default Editor") {
-          Picker(
-            "Default editor",
-            selection: $store.defaultEditorID
-          ) {
-            Text("Automatic")
-              .tag(OpenWorktreeAction.automaticSettingsID)
-            ForEach(openActionOptions) { action in
-              Text(action.labelTitle)
-                .tag(action.settingsID)
-            }
+      }
+      Section {
+        Toggle(
+          "Confirm before Quitting",
+          isOn: $store.confirmBeforeQuit
+        )
+        .help("Ask before quitting Supacode")
+      }
+      Section("Editor") {
+        Picker(
+          selection: $store.defaultEditorID
+        ) {
+          Text("Automatic")
+            .tag(OpenWorktreeAction.automaticSettingsID)
+          ForEach(openActionOptions) { action in
+            Text(action.labelTitle)
+              .tag(action.settingsID)
           }
-          .help("Applies to worktrees without repository overrides.")
-        }
-        Section("Quit") {
-          Toggle(
-            "Confirm before quitting",
-            isOn: $store.confirmBeforeQuit
-          )
-          .help("Ask before quitting Supacode")
+        } label: {
+          Text("Default Editor")
+          Text("Applies to Worktrees without repository overrides.")
         }
       }
-      .formStyle(.grouped)
+      Section {
+        Toggle(isOn: $store.analyticsEnabled) {
+          Text("Share Analytics")
+          Text("Anonymous usage data helps improve Supacode.")
+        }
+        Toggle(isOn: $store.crashReportsEnabled) {
+          Text("Share Crash Reports")
+          Text("Anonymous crash reports help improve stability.")
+        }
+      } header: {
+        Text("Analytics")
+      } footer: {
+        Text("Changes to Analytics require Supacode to restart before they take effect.")
+      }
     }
-    .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
+    .formStyle(.grouped)
+    .padding(.top, -20)
+    .padding(.leading, -8)
+    .padding(.trailing, -6)
+
+    .navigationTitle("General")
   }
 }

supacode/Features/Settings/Views/WorktreeSettingsView.swift
@@ -5,59 +5,44 @@ struct WorktreeSettingsView: View {
   @Bindable var store: StoreOf<SettingsFeature>
 
   var body: some View {
-    let exampleRepositoryRoot = FileManager.default.homeDirectoryForCurrentUser
-      .appending(path: "code/my-repo", directoryHint: .isDirectory)
-    let exampleWorktreePath = SupacodePaths.exampleWorktreePath(
-      for: exampleRepositoryRoot,
-      globalDefaultPath: store.defaultWorktreeBaseDirectoryPath,
-      repositoryOverridePath: nil
-    )
-    VStack(alignment: .leading) {
-      Form {
-        Section("Worktree") {
-          VStack(alignment: .leading) {
-            TextField(
-              "Default: current behavior",
-              text: $store.defaultWorktreeBaseDirectoryPath
-            )
-            .textFieldStyle(.roundedBorder)
-            Text("Default directory for new worktrees across repositories. Leave empty to keep current behavior.")
-              .foregroundStyle(.secondary)
-            Text("Example new worktree path: \(exampleWorktreePath)")
-              .foregroundStyle(.secondary)
-              .monospaced()
-          }
-          .frame(maxWidth: .infinity, alignment: .leading)
-          VStack(alignment: .leading) {
-            Toggle(
-              "Also delete local branch when deleting a worktree",
-              isOn: $store.deleteBranchOnDeleteWorktree
-            )
-            .help("Delete the local branch when deleting a worktree")
-            Text("Removes the local branch along with the worktree. Remote branches must be deleted on GitHub.")
-              .foregroundStyle(.secondary)
-            Text("Uncommitted changes will be lost.")
-              .foregroundStyle(.red)
-          }
-          .frame(maxWidth: .infinity, alignment: .leading)
-          Toggle(
-            "Automatically archive merged worktrees",
-            isOn: $store.automaticallyArchiveMergedWorktrees
-          )
-          .help("Archive worktrees automatically when their pull requests are merged.")
-          VStack(alignment: .leading) {
-            Toggle(
-              "Prompt for branch name during creation",
-              isOn: $store.promptForWorktreeCreation
-            )
-            .help("Ask for branch name and base ref before creating a worktree.")
-            Text("When enabled, you choose the branch name and where it branches from before creating the worktree.")
-              .foregroundStyle(.secondary)
-          }
+    let defaultPath = SupacodePaths.reposDirectory.path(percentEncoded: false)
+    let resolvedBase =
+      SupacodePaths.normalizedWorktreeBaseDirectoryPath(
+        store.defaultWorktreeBaseDirectoryPath
+      ) ?? defaultPath
+    let examplePath = "\(resolvedBase)*/**/*"
+    Form {
+      Section {
+        Toggle(isOn: $store.promptForWorktreeCreation) {
+          Text("Prompt for branch name on creation")
+          Text("Choose the branch name and base ref before creating the worktree.")
+        }
+        TextField(
+          text: $store.defaultWorktreeBaseDirectoryPath,
+          prompt: Text(defaultPath)
+        ) {
+          Text("Default directory").monospaced(false)
+          Text("Parent path for new worktrees.").monospaced(false)
+        }.monospaced()
+      } footer: {
+        Text("e.g., `\(examplePath)`")
+      }
+      Section("Clean-up") {
+        Toggle(isOn: $store.automaticallyArchiveMergedWorktrees) {
+          Text("Automatically archive merged worktrees")
+          Text("Archives worktrees when their pull requests are merged.")
+        }
+        Toggle(isOn: $store.deleteBranchOnDeleteWorktree) {
+          Text("Delete local branch with worktree")
+          Text("Removes the local branch along with the worktree. Remote branches must be deleted on GitHub.")
+          Text("Uncommitted changes will be lost.").foregroundStyle(.red)
         }
       }
-      .formStyle(.grouped)
     }
-    .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
+    .formStyle(.grouped)
+    .padding(.top, -20)
+    .padding(.leading, -8)
+    .padding(.trailing, -6)
+    .navigationTitle("Worktrees")
   }
 }

supacode/Infrastructure/Ghostty/GhosttyRuntime.swift
@@ -1,9 +1,12 @@
 import AppKit
 import GhosttyKit
+import Sharing
 import SwiftUI
 import UniformTypeIdentifiers
 
 final class GhosttyRuntime {
+  private static let logger = SupaLogger("Ghostty")
+
   final class SurfaceReference {
     let surface: ghostty_surface_t
     var isValid = true
@@ -141,6 +144,26 @@ final class GhosttyRuntime {
   func unregisterSurface(_ ref: SurfaceReference) {
     ref.invalidate()
     surfaceRefs = surfaceRefs.filter { $0.isValid }
+  }
+
+  /// Reloads the full app config from disk and re-applies the current color scheme.
+  func reloadAppConfig() {
+    guard let app else {
+      Self.logger.warning("Cannot reload app config: Ghostty app instance is nil.")
+      return
+    }
+    var target = ghostty_target_s()
+    target.tag = GHOSTTY_TARGET_APP
+    guard let config = Self.loadConfig() else {
+      Self.logger.warning("Failed to reload app config.")
+      return
+    }
+    applyConfig(config, target: target, app: app)
+    ghostty_config_free(config)
+    if let lastColorScheme {
+      ghostty_app_set_color_scheme(app, lastColorScheme)
+      applyColorSchemeToSurfaces(lastColorScheme)
+    }
   }
 
   func reloadConfig(soft: Bool, target: ghostty_target_s) {
@@ -440,12 +463,51 @@ final class GhosttyRuntime {
   }
 
   private static func loadConfig() -> ghostty_config_t? {
+    @Shared(.settingsFile) var settingsFile
     guard let config = ghostty_config_new() else { return nil }
     ghostty_config_load_default_files(config)
     ghostty_config_load_recursive_files(config)
     ghostty_config_load_cli_args(config)
+    loadBundledOverrides(into: config)
+    loadBundledTheme(into: config, enabled: settingsFile.global.terminalThemeSyncEnabled)
     ghostty_config_finalize(config)
     return config
+  }
+
+  /// Applies Supacode-specific config (padding values) that takes precedence over user settings.
+  private static func loadBundledOverrides(into config: ghostty_config_t) {
+    let defaults = "window-padding-x = 14\nwindow-padding-y = 12,0\n"
+    let tempURL = FileManager.default.temporaryDirectory.appendingPathComponent("supacode-defaults.conf")
+    do {
+      try defaults.write(to: tempURL, atomically: true, encoding: .utf8)
+    } catch {
+      logger.warning("Failed to write bundled defaults: \(error.localizedDescription)")
+      return
+    }
+    tempURL.path.withCString { ghostty_config_load_file(config, $0) }
+  }
+
+  /// When terminal theme sync is enabled, loads the bundled Supacode
+  /// light/dark theme, overriding any user-configured theme. When disabled, the user's Ghostty theme is preserved.
+  private static func loadBundledTheme(into config: ghostty_config_t, enabled: Bool) {
+    guard enabled else { return }
+    guard
+      let lightPath = Bundle.main.path(forResource: "Supacode Light", ofType: nil),
+      let darkPath = Bundle.main.path(forResource: "Supacode Dark", ofType: nil)
+    else {
+      assertionFailure("Bundled Supacode themes missing from app bundle.")
+      logger.warning("Bundled Supacode themes missing from app bundle.")
+      return
+    }
+    let line = "theme = light:\(lightPath),dark:\(darkPath)\n"
+    let tempURL = FileManager.default.temporaryDirectory.appendingPathComponent("supacode-theme.conf")
+    do {
+      try line.write(to: tempURL, atomically: true, encoding: .utf8)
+    } catch {
+      logger.warning("Failed to write bundled theme config: \(error.localizedDescription)")
+      return
+    }
+    tempURL.path.withCString { ghostty_config_load_file(config, $0) }
   }
 
   func keyboardShortcut(for action: String) -> KeyboardShortcut? {

pacificleo/prs-code

cherrylily/Features/Settings/Views/AppearanceSettingsView.swift
@@ -6,66 +6,74 @@ struct AppearanceSettingsView: View {
 
   var body: some View {
     let openActionOptions = OpenWorktreeAction.availableCases(settings: store.state.globalSettings)
-    VStack(alignment: .leading) {
-      Form {
-        Section("Appearance") {
-          HStack {
+    Form {
+      Section {
+        LabeledContent("Appearance") {
+          HStack(spacing: 12) {
             let appearanceMode = $store.appearanceMode
             ForEach(AppearanceMode.allCases) { mode in
               AppearanceOptionCardView(
                 mode: mode,
-                isSelected: mode == appearanceMode.wrappedValue
-              ) {
-                appearanceMode.wrappedValue = mode
-              }
+                isSelected: mode == appearanceMode.wrappedValue,
+                action: { appearanceMode.wrappedValue = mode }
+              )
             }
           }
+        }
+        Toggle(isOn: $store.terminalThemeSyncEnabled) {
+          Text("Sync with Terminal")
+          Text("Applies the appearance-aware CherryLily color palette.")
+        }
+        if !store.terminalThemeSyncEnabled {
           VStack(alignment: .leading, spacing: 4) {
-            Text("Terminal theming follows Ghostty config")
-            Text("For example, add the following line to `~/.config/ghostty/config`")
-            Text("theme = light:Monokai Pro Light Sun,dark:Dimmed Monokai")
-              .monospaced()
+            Text("Add a theme to `~/.config/ghostty/config`")
+            Text("e.g. `theme = light:Monokai Pro Light Sun,dark:Dimmed Monokai`")
           }
           .font(.footnote)
           .foregroundStyle(.secondary)
           .textSelection(.enabled)
         }
-        Section("Default Editor") {
-          Picker(
-            "Default editor",
-            selection: $store.defaultEditorID
-          ) {
-            Text("Automatic")
-              .tag(OpenWorktreeAction.automaticSettingsID)
-            ForEach(openActionOptions) { action in
-              Text(action.labelTitle)
-                .tag(action.settingsID)
-            }
+      }
+      Section {
+        Toggle(
+          "Confirm before Quitting",
+          isOn: $store.confirmBeforeQuit
+        )
+        .help("Ask before quitting CherryLily")
+        Toggle(
+          "Confirm before closing terminal tabs",
+          isOn: $store.confirmBeforeClosingTabs
+        )
+        .help("Show a confirmation dialog when closing a tab")
+      }
+      Section("Keyboard") {
+        Toggle(
+          "Show shortcut hints when holding Command or Control",
+          isOn: $store.showShortcutHints
+        )
+        .help("Show keyboard shortcut hints on worktree rows, tabs, and buttons when holding modifier keys")
+      }
+      Section("Editor") {
+        Picker(
+          selection: $store.defaultEditorID
+        ) {
+          Text("Automatic")
+            .tag(OpenWorktreeAction.automaticSettingsID)
+          ForEach(openActionOptions) { action in
+            Text(action.labelTitle)
+              .tag(action.settingsID)
           }
-          .help("Applies to worktrees without repository overrides.")
-        }
-        Section("Keyboard") {
-          Toggle(
-            "Show shortcut hints when holding Command or Control",
-            isOn: $store.showShortcutHints
-          )
-          .help("Show keyboard shortcut hints on worktree rows, tabs, and buttons when holding modifier keys")
-        }
-        Section("Quit") {
-          Toggle(
-            "Confirm before quitting",
-            isOn: $store.confirmBeforeQuit
-          )
-          .help("Ask before quitting CherryLily")
-          Toggle(
-            "Confirm before closing terminal tabs",
-            isOn: $store.confirmBeforeClosingTabs
-          )
-          .help("Show a confirmation dialog when closing a tab")
+        } label: {
+          Text("Default Editor")
+          Text("Applies to Worktrees without repository overrides.")
         }
       }
-      .formStyle(.grouped)
     }
-    .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
+    .formStyle(.grouped)
+    .padding(.top, -20)
+    .padding(.leading, -8)
+    .padding(.trailing, -6)
+
+    .navigationTitle("General")
   }
 }

cherrylily/Features/Settings/Views/WorktreeSettingsView.swift
@@ -5,59 +5,44 @@ struct WorktreeSettingsView: View {
   @Bindable var store: StoreOf<SettingsFeature>
 
   var body: some View {
-    let exampleRepositoryRoot = FileManager.default.homeDirectoryForCurrentUser
-      .appending(path: "code/my-repo", directoryHint: .isDirectory)
-    let exampleWorktreePath = CherryLilyPaths.exampleWorktreePath(
-      for: exampleRepositoryRoot,
-      globalDefaultPath: store.defaultWorktreeBaseDirectoryPath,
-      repositoryOverridePath: nil
-    )
-    VStack(alignment: .leading) {
-      Form {
-        Section("Worktree") {
-          VStack(alignment: .leading) {
-            TextField(
-              "Default: current behavior",
-              text: $store.defaultWorktreeBaseDirectoryPath
-            )
-            .textFieldStyle(.roundedBorder)
-            Text("Default directory for new worktrees across repositories. Leave empty to keep current behavior.")
-              .foregroundStyle(.secondary)
-            Text("Example new worktree path: \(exampleWorktreePath)")
-              .foregroundStyle(.secondary)
-              .monospaced()
-          }
-          .frame(maxWidth: .infinity, alignment: .leading)
-          VStack(alignment: .leading) {
-            Toggle(
-              "Also delete local branch when deleting a worktree",
-              isOn: $store.deleteBranchOnDeleteWorktree
-            )
-            .help("Delete the local branch when deleting a worktree")
-            Text("Removes the local branch along with the worktree. Remote branches must be deleted on GitHub.")
-              .foregroundStyle(.secondary)
-            Text("Uncommitted changes will be lost.")
-              .foregroundStyle(.red)
-          }
-          .frame(maxWidth: .infinity, alignment: .leading)
-          Toggle(
-            "Automatically archive merged worktrees",
-            isOn: $store.automaticallyArchiveMergedWorktrees
-          )
-          .help("Archive worktrees automatically when their pull requests are merged.")
-          VStack(alignment: .leading) {
-            Toggle(
-              "Prompt for branch name during creation",
-              isOn: $store.promptForWorktreeCreation
-            )
-            .help("Ask for branch name and base ref before creating a worktree.")
-            Text("When enabled, you choose the branch name and where it branches from before creating the worktree.")
-              .foregroundStyle(.secondary)
-          }
+    let defaultPath = CherryLilyPaths.reposDirectory.path(percentEncoded: false)
+    let resolvedBase =
+      CherryLilyPaths.normalizedWorktreeBaseDirectoryPath(
+        store.defaultWorktreeBaseDirectoryPath
+      ) ?? defaultPath
+    let examplePath = "\(resolvedBase)*/**/*"
+    Form {
+      Section {
+        Toggle(isOn: $store.promptForWorktreeCreation) {
+          Text("Prompt for branch name on creation")
+          Text("Choose the branch name and base ref before creating the worktree.")
+        }
+        TextField(
+          text: $store.defaultWorktreeBaseDirectoryPath,
+          prompt: Text(defaultPath)
+        ) {
+          Text("Default directory").monospaced(false)
+          Text("Parent path for new worktrees.").monospaced(false)
+        }.monospaced()
+      } footer: {
+        Text("e.g., `\(examplePath)`")
+      }
+      Section("Clean-up") {
+        Toggle(isOn: $store.automaticallyArchiveMergedWorktrees) {
+          Text("Automatically archive merged worktrees")
+          Text("Archives worktrees when their pull requests are merged.")
+        }
+        Toggle(isOn: $store.deleteBranchOnDeleteWorktree) {
+          Text("Delete local branch with worktree")
+          Text("Removes the local branch along with the worktree. Remote branches must be deleted on GitHub.")
+          Text("Uncommitted changes will be lost.").foregroundStyle(.red)
         }
       }
-      .formStyle(.grouped)
     }
-    .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
+    .formStyle(.grouped)
+    .padding(.top, -20)
+    .padding(.leading, -8)
+    .padding(.trailing, -6)
+    .navigationTitle("Worktrees")
   }
 }

cherrylily/Infrastructure/Ghostty/GhosttyRuntime.swift
@@ -1,9 +1,12 @@
 import AppKit
 import GhosttyKit
+import Sharing
 import SwiftUI
 import UniformTypeIdentifiers
 
 final class GhosttyRuntime {
+  private static let logger = SupaLogger("Ghostty")
+
   final class SurfaceReference: Hashable {
     let surface: ghostty_surface_t
     var isValid = true
@@ -152,6 +155,26 @@ final class GhosttyRuntime {
   func unregisterSurface(_ ref: SurfaceReference) {
     ref.invalidate()
     surfaceRefs.remove(ref)
+  }
+
+  /// Reloads the full app config from disk and re-applies the current color scheme.
+  func reloadAppConfig() {
+    guard let app else {
+      Self.logger.warning("Cannot reload app config: Ghostty app instance is nil.")
+      return
+    }
+    var target = ghostty_target_s()
+    target.tag = GHOSTTY_TARGET_APP
+    guard let config = Self.loadConfig() else {
+      Self.logger.warning("Failed to reload app config.")
+      return
+    }
+    applyConfig(config, target: target, app: app)
+    ghostty_config_free(config)
+    if let lastColorScheme {
+      ghostty_app_set_color_scheme(app, lastColorScheme)
+      applyColorSchemeToSurfaces(lastColorScheme)
+    }
   }
 
   func reloadConfig(soft: Bool, target: ghostty_target_s) {
@@ -451,12 +474,51 @@ final class GhosttyRuntime {
   }
 
   private static func loadConfig() -> ghostty_config_t? {
+    @Shared(.settingsFile) var settingsFile
     guard let config = ghostty_config_new() else { return nil }
     ghostty_config_load_default_files(config)
     ghostty_config_load_recursive_files(config)
     ghostty_config_load_cli_args(config)
+    loadBundledOverrides(into: config)
+    loadBundledTheme(into: config, enabled: settingsFile.global.terminalThemeSyncEnabled)
     ghostty_config_finalize(config)
     return config
+  }
+
+  /// Applies CherryLily-specific config (padding values) that takes precedence over user settings.
+  private static func loadBundledOverrides(into config: ghostty_config_t) {
+    let defaults = "window-padding-x = 14\nwindow-padding-y = 12,0\n"
+    let tempURL = FileManager.default.temporaryDirectory.appendingPathComponent("cherrylily-defaults.conf")
+    do {
+      try defaults.write(to: tempURL, atomically: true, encoding: .utf8)
+    } catch {
+      logger.warning("Failed to write bundled defaults: \(error.localizedDescription)")
+      return
+    }
+    tempURL.path.withCString { ghostty_config_load_file(config, $0) }
+  }
+
+  /// When terminal theme sync is enabled, loads the bundled CherryLily
+  /// light/dark theme, overriding any user-configured theme. When disabled, the user's Ghostty theme is preserved.
+  private static func loadBundledTheme(into config: ghostty_config_t, enabled: Bool) {
+    guard enabled else { return }
+    guard
+      let lightPath = Bundle.main.path(forResource: "CherryLily Light", ofType: nil),
+      let darkPath = Bundle.main.path(forResource: "CherryLily Dark", ofType: nil)
+    else {
+      assertionFailure("Bundled Supacode themes missing from app bundle.")
+      logger.warning("Bundled Supacode themes missing from app bundle.")
+      return
+    }
+    let line = "theme = light:\(lightPath),dark:\(darkPath)\n"
+    let tempURL = FileManager.default.temporaryDirectory.appendingPathComponent("cherrylily-theme.conf")
+    do {
+      try line.write(to: tempURL, atomically: true, encoding: .utf8)
+    } catch {
+      logger.warning("Failed to write bundled theme config: \(error.localizedDescription)")
+      return
+    }
+    tempURL.path.withCString { ghostty_config_load_file(config, $0) }
   }
 
   func keyboardShortcut(for action: String) -> KeyboardShortcut? {