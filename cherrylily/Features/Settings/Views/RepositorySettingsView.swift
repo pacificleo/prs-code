benlenarts/supacode

Fix repo default directory prompt to show resolved path
Show the actual default worktree base directory for the specific
repository (including global default + repo name) instead of the
generic repos directory.

supacode/Features/Settings/Views/RepositorySettingsView.swift
@@ -64,7 +64,13 @@ struct RepositorySettingsView: View {
         }
         TextField(
           text: worktreeBaseDirectoryPath,
-          prompt: Text(SupacodePaths.reposDirectory.path(percentEncoded: false))
+          prompt: Text(
+            SupacodePaths.worktreeBaseDirectory(
+              for: store.rootURL,
+              globalDefaultPath: store.globalDefaultWorktreeBaseDirectoryPath,
+              repositoryOverridePath: nil
+            ).path(percentEncoded: false)
+          )
         ) {
           Text("Default directory").monospaced(false)
           Text("Parent path for new worktrees.").monospaced(false)

pacificleo/prs-code

cherrylily/Features/Settings/Views/RepositorySettingsView.swift
@@ -64,7 +64,13 @@ struct RepositorySettingsView: View {
         }
         TextField(
           text: worktreeBaseDirectoryPath,
-          prompt: Text(CherryLilyPaths.reposDirectory.path(percentEncoded: false))
+          prompt: Text(
+            CherryLilyPaths.worktreeBaseDirectory(
+              for: store.rootURL,
+              globalDefaultPath: store.globalDefaultWorktreeBaseDirectoryPath,
+              repositoryOverridePath: nil
+            ).path(percentEncoded: false)
+          )
         ) {
           Text("Default directory").monospaced(false)
           Text("Parent path for new worktrees.").monospaced(false)